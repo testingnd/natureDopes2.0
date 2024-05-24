55ac264a9ca519045036c21dc15a77ed1cf371c4
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-05-24 11:36:59 +0200

    add radix ui to register page. need to correct sizing/ positioning

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 
 import {registerUser} from './_registerPageAction'
 
+import { Theme } from "@radix-ui/themes";
+import { Card, Flex, Button, TextField } from "@radix-ui/themes";
+import {SubmitButton} from '../components/buttons/SubmitButton' 
 import styles from './register.module.css'
 
 
@@ -18,27 +21,30 @@ export default function RegisterPage(){
     }
 
     return(
-
-        <div>
-            <div className={styles.createAccount}>
-                <h1> Create your account </h1>
-                <form className={styles.registerForm} action={submit}>
-                    <input className={styles.input} placeholder="Username" name='Username' />
-                    <input className={styles.input} placeholder="Email" name='Email' type='email' />
-                    <input className={styles.input} placeholder="Password" name='Password' type='password' />
-                    <input className={styles.input} placeholder="Confirm Password" name='Confirm' type='password' />
-                    <button type='submit'>Register</button>
-                </form>
-
-            </div>
-            {error && <p>{error}</p>}
-            
-            <div className={styles.backToSignIn}>
-                <h3>Have an account already?</h3>
-                <Link href='/api/auth/signin'>Sign in</Link>
-            </div>
-
-        </div>
+        <main>
+        <Theme accentColor="grass" >    
+            <Card>
+                <Flex gap='3' direction='column' >
+                    <h1> Create your account </h1>
+                    <Flex gap ='2' direction='column' asChild>
+                        <form action={submit}>
+                            <TextField.Root size='3' placeholder="Username" name='Username' />
+                            <TextField.Root size='3' placeholder="Email" name='Email' type='email' />
+                            <TextField.Root size='3' placeholder="Password" name='Password' type='password' />
+                            <TextField.Root size='3' placeholder="Confirm Password" name='Confirm' type='password' />
+                            <SubmitButton>Register</SubmitButton>
+                        </form>
+                    </Flex>        
+                </Flex>
+                {error && <p className={styles.errorMessages}>{error}</p>}
+                            
+                <div className={styles.backToSignIn}>
+                    <h3>Have an account already?</h3>
+                    <Link href='/api/auth/signin'>Sign in</Link>
+                </div>
+            </Card>
+        </Theme> 
+        </main>    
 
     )
 }
