1cbe2b2a10a858e33de670ad7193a46519284d62
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-06-06 14:45:05 +0200

    changed forgotpassword route to ssr page in order to add session check before rendering form

---
+++ app/forgotPassword/ForgotPasswordForm.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useState } from "react";
+
+import {sendPasswordReset} from './_sendPasswordReset'
+
+import { Theme } from "@radix-ui/themes";
+import { Card, Flex, Button, TextField } from "@radix-ui/themes";
+import {SubmitButton} from '../_components/buttons/SubmitButton' 
+import styles from './passwordStyles.module.css'
+
+
+
+export default  function ForgotPassword(){
+
+
+
+    const [error, setError] = useState<string>('')
+
+    const submit = async (data: FormData) => {
+        const { error } = await sendPasswordReset(data)
+        setError(error)
+      }
+
+    return(
+        
+        
+   
+         <Theme accentColor="grass" >     
+            <Card>
+                <Flex gap='4' direction='column' asChild>
+                    <form action={submit}>
+                        <h1> Reset Password</h1>
+                        <p>Enter your email address here, a link will be sent to your inbox, click the link to reset your password</p>
+
+                        <TextField.Root 
+                        name='email' 
+                        type='email' 
+                        size='3' 
+                        placeholder='Your email...'
+                        />
+                        {error && <p className={styles.errorMessage}>{error}</p>}
+
+                        <SubmitButton>Send</SubmitButton>
+                        
+                    </form>
+
+
+                </Flex>
+
+            </Card>
+        </Theme> 
+      
+       
+    )
+}
--- app/forgotPassword/page.tsx
+++ app/forgotPassword/page.tsx
@@ -1,58 +1,25 @@
-'use client'
 
-import Link from "next/link";
-import { useState } from "react";
 
-import {sendPasswordReset} from './_sendPasswordReset'
-
-import { Theme } from "@radix-ui/themes";
-import { Card, Flex, Button, TextField } from "@radix-ui/themes";
-import {SubmitButton} from '../_components/buttons/SubmitButton' 
-import styles from './passwordStyles.module.css'
+import React from "react"
+import ForgotPassword from "./ForgotPasswordForm"
 
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/route'
 
 
+const session: any = await getServerSession(authOptions)
 
-export default function ForgotPassword(){
 
- 
+export default  function ForgotPasswordPage(){
 
-    const [error, setError] = useState<string>('')
 
-    const submit = async (data: FormData) => {
-        const { error } = await sendPasswordReset(data)
-        setError(error)
-      }
+
 
     return(
         
         <main>
-         <Theme accentColor="grass" >     
-            <Card>
-                <Flex gap='4' direction='column' asChild>
-                    <form action={submit}>
-                        <h1> Reset Password</h1>
-                        <p>Enter your email address here, a link will be sent to your inbox, click the link to reset your password</p>
-
-                        <TextField.Root 
-                        name='email' 
-                        type='email' 
-                        size='3' 
-                        placeholder='Your email...'
-                        />
-                        {error && <p className={styles.errorMessage}>{error}</p>}
-
-                        <SubmitButton>Send</SubmitButton>
-                        
-                    </form>
-
-
-                </Flex>
-
-            </Card>
-        </Theme>
+            {!session? <ForgotPassword/>: <p>Already logged in as {session.user.name} please return to the main site</p>}
+   
         </main>
        
     )
