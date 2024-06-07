4b9cdfb30e40077c58e155536560626c4c3b2155
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-06-07 10:20:13 +0200

    added further redirects to forgotpassword sub routes to handle any routing errors after login

--- app/forgotPassword/success/page.tsx
+++ app/forgotPassword/success/page.tsx
@@ -1,8 +1,20 @@
 
 
-export default function ResetSuccess(){
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../api/auth/[...nextauth]/route'
+
+import { redirect } from "next/navigation";
+
+
+export default async function ResetSuccess(){
+
+    const session: any = await getServerSession(authOptions)
+
+    if(session){
+        redirect('/')
+    }
 
     return(
-        <p>ThANKS, RESET SUCCESS</p>
+        <p>Thanks, we have sent an email to your account. Click on the link in the email to reset your password</p>
     )
 }
--- app/forgotPassword/successpassReset/page.tsx
+++ app/forgotPassword/successpassReset/page.tsx
@@ -1,7 +1,18 @@
 
-export default function ResetSuccess(){
+import { redirect } from "next/navigation"
+
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../api/auth/[...nextauth]/route'
+
+export default async function ResetSuccess(){
+
+    const session: any = await getServerSession(authOptions)
+
+    if(session){
+        redirect('/')
+    }
 
     return(
-        <p>ThANKS, password has been reset</p>
+        <p>Thanks, your password has been reset</p>
     )
 }
