bd248d0da9eebeb103d3fbecbe0081f37da4bb19
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-06-14 15:01:29 +0200

    added state for toggling view of upload form, will display in map section

--- app/map/_components/GMap.tsx
+++ app/map/_components/GMap.tsx
@@ -12,23 +12,32 @@ import Loading from '@/app/loading'
 import { StaticImageData } from 'next/image';
 
 
-import { Switch, Theme } from '@radix-ui/themes';
-
+import { Button, Switch, Theme } from '@radix-ui/themes';
+import style from './mapMarker.module.css'
 
 
 
 export default function Gmap({getImageData, loadingGif, session}: {getImageData: ImageData, loadingGif: StaticImageData, session: number}) {
 
-
+  //state for image file collected from iagon api
   const [imageData, setImageData] = useState<ImageData>(getImageData)
 
+  // state for search bar
   const [searchParams, setSearchParams] = useState<string>('')
 
+  //toggles if user would like to see all markers or just there own
   const [allChecked, setAllChecked]  = useState<boolean>(true)
 
   // state for long/ lat positons for onclickMap function
   const[gps_lat, setLat] = useState<number>(0)
   const[gps_long, setLong] = useState<number>(0)
+
+  // whether upload form is visible
+  const[uploadForm, setUploadForm]= useState<boolean>(false)
+
+  function toggleUploadForm(){
+    setUploadForm(!uploadForm)
+  }
 
 
 
@@ -58,17 +67,25 @@ function onClickMap({lat, lng}) {
   return (
     // Important! Always set the container height explicitly
     <>
-       <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
 
-      <label >Search</label><br />
-        <input name='searchSpecies' type='text' placeholder="Search for species here... " onChange={event => setSearchParams(event.target.value)}/>
+        <label >Search</label><br />
+          <input name='searchSpecies' type='text' placeholder="Search for species here... " onChange={event => setSearchParams(event.target.value)}/>
       </form>
 
     <Theme accentColor='grass' data-is-root-theme='false'>
-      { !session ? null :<><label>All finds</label>
+      { !session ? null :
+      
+      <>
+      <label>All finds</label>
       <Switch checked={!allChecked} onCheckedChange={() => setAllChecked(allChecked => !allChecked)} />
-      <label>Your finds</label></>
+      <label>Your finds</label>
+
+        {uploadForm ? <Button onClick={toggleUploadForm}>+</Button>: null}
+      </>
       }
+
+
      
 
     </Theme>
@@ -78,7 +95,7 @@ function onClickMap({lat, lng}) {
       
       <div style={{ height: '90vh', width: '100%' }}>
 
-        
+        {uploadForm? null: <ImageUploadForm lng={gps_long} lat={gps_lat} session={session} toggleUploadForm={toggleUploadForm}/>}
         <Suspense fallback={<Loading/>}>
         <GoogleMapReact
           bootstrapURLKeys={{ key:  process.env.NEXT_PUBLIC_GOOGLEMAPAPI}}
@@ -87,7 +104,7 @@ function onClickMap({lat, lng}) {
           yesIWantToUseGoogleMapApiInternals
           onClick={onClickMap}
         >
-        <ImageUploadForm lng={gps_long} lat={gps_lat} session={session}/>
+     
     
         {imageData.filter(data => {
           if(!allChecked){
--- app/map/_components/forms/imageUploadForm.tsx
+++ app/map/_components/forms/imageUploadForm.tsx
@@ -4,6 +4,7 @@ import {useState} from "react";
 
 import { Theme } from "@radix-ui/themes";
 import { Card, Flex, Button, TextField } from "@radix-ui/themes";
+import style from './uploadForm.module.css'
 
 import { iagonUpload } from "../../_lib/uploadImageIagon";
 import { registerImageData } from "../../_lib/registerImageData";
@@ -11,7 +12,7 @@ import { registerImageData } from "../../_lib/registerImageData";
 import { SubmitButton } from "@/app/_components/buttons/SubmitButton";
 
 
-export default function imageUploadForm({lng, lat, session}: {lng: number, lat: number, session: number}){
+export default function imageUploadForm({lng, lat, session, toggleUploadForm}: {lng: number, lat: number, session: number, toggleUploadForm: Function}){
 
     const [ error, setError] = useState<string>('')
     const [success, setSuccess] = useState<string>('')
@@ -39,14 +40,15 @@ export default function imageUploadForm({lng, lat, session}: {lng: number, lat: 
 
 
     return(
-
+        <section className={style.uploadFormWrapper}>
         <Theme accentColor="grass" data-is-root-theme='false'>
             <Card >
                 <Flex width='50%' gap='4' direction='column'>
+                    <Button onClick={toggleUploadForm}>close</Button>
                     <form action={submit}>
                         <TextField.Root name='species' placeholder="Species Name" size='3'  />
-                        <TextField.Root name='gps_long' placeholder="Position Long" size='3' defaultValue={lng} />
-                        <TextField.Root name='gps_lat' placeholder="Position Lat" size='3' defaultValue={lat}/>
+                        <TextField.Root name='gps_long' placeholder="Position Long" size='3' value={lng} />
+                        <TextField.Root name='gps_lat' placeholder="Position Lat" size='3' value={lat}/>
                         <input name='image_file' placeholder="Image" type="file" />   
                         <SubmitButton>Upload</SubmitButton>
                     </form>
@@ -56,6 +58,6 @@ export default function imageUploadForm({lng, lat, session}: {lng: number, lat: 
                 </Flex>
             </Card>
         </Theme>
-
+        </section>
     )
 }
