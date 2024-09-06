5edc958e5cc5fde8c0486d4fe94faba8a17e7c36
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-09-06 10:28:05 +0200

    added favicons, updated styling to text in finder page

--- src/app/[locale]/finder/page.tsx
+++ src/app/[locale]/finder/page.tsx
@@ -9,10 +9,10 @@ import Image from 'next/image'
 import ndLogo from '../../../../public/images/logo_mid.png'
 
 import { Indie_Flower } from 'next/font/google'
-import GoButton from '../_components/buttons/GoButton'
 
 
-import { Button } from '@radix-ui/themes'
+import { Button, Text} from '@radix-ui/themes'
+import { SubmitButton } from '../_components/buttons/SubmitButton'
 
 
 
@@ -50,12 +50,12 @@ export default  function Page() {
         </section>
         
         
-        <section className={`${indie.className} ${styles.rootPageSection} `}>
-            <h1> Welcome to the Nature Dopes mini-game <span style={{color: '#5B9240', fontWeight: '800'}}>'Flower finder'</span></h1>
-            <h2> <br /> While out in Nature, see if you can find each flower then answer a question for each</h2>
-            <h3> <br /> Click Go to begin </h3>
+        <section className={styles.rootPageSection}>
+            <Text size='4'> Welcome to the Nature Dopes mini-game <span className={indie.className} style={{color: '#5B9240', fontWeight: '800', fontSize: '1.1em'}}>'Flower finder'</span></Text>
+            <Text size='4'> <br /> While out in Nature, see if you can find each flower then answer a question for each</Text>
+            <Text size='4'> <br /> Click Go to begin </Text>
             <Link className={styles.finderLink} href="/finder/game">
-              <Button>Go!</Button>
+              <SubmitButton>Go!</SubmitButton>
             </Link>
               
               
