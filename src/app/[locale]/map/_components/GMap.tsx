bdc92dc33730f81c8853afaa39c948c10eb5c534
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-09-20 15:54:25 +0200

    updating process.env variable use for switch to live site

--- src/app/[locale]/map/_components/GMap.tsx
+++ src/app/[locale]/map/_components/GMap.tsx
@@ -56,7 +56,7 @@ export default function Gmap({getImageData, loadingGif, session, translationProp
   const[editForm, setEditForm] = useState<boolean>(false)
 
   async function getData() {
-    const res = await fetch('http://localhost:3000/map/api')
+    const res = await fetch(`${process.env.LIVESITE}map/api`)
   
     if (!res.ok) {
      
